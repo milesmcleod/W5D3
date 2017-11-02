981837112d93c1a4a2bf836de26cb5c395effd16
Miles McLeod <milesmcleod123@gmail.com>
2017-11-01 17:28:05 -0700

    Complete bonus phase 9

--- rails_lite/lib/show_exceptions.rb
+++ rails_lite/lib/show_exceptions.rb
@@ -1,15 +1,29 @@
 require 'erb'
 
 class ShowExceptions
+
+  attr_accessor :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      app.call(env)
+    rescue Exception => e
+      render_exception(e)
+    end
   end
 
   private
 
   def render_exception(e)
+    @e = e
+    template = File.read('lib/templates/rescue.html.erb')
+    content = ERB.new(template).result(binding)
+    res = ["500", {'Content-type' => 'text/html'}, content]
+    res
   end
 
 end
